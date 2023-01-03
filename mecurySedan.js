7c5f110109e9223e518531cd95d1816ed0085427
Zane Matero <zanematero@Zanes-MacBook-Pro.local>
2023-01-03 11:19:15 -0800

    js methods and inheritence

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,41 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require('./vehicle').Vehicle
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+class Car extends VehicleModule {
+    constructor (make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    service() {
+        if (this.mileage > 30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true
+        }
+    }
+
+    load(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num += this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;
+            }
+        }
+    }
+}
+
+let myCar = new Car('BMW', 'X5', '2021', 'red', 10000)
+
+myCar.start()
+myCar.load(3)
+myCar.service()
